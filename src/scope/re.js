f98c213fe54a834291ddad439725cfb6e7235623
Daniel Cardenas Parra <danielcardenasparra@gmail.com>
2022-09-12 17:17:46 -0500

    agregamos clase de redeclarar y strictmode

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+//Var
+var firstName ; // Undefined
+firstName = 'Daniel';
+console.log(firstName);
+
+var lastName = 'Cardenas'; //Declarar / Asignar
+lastName = 'Parra'; // Reasignar
+var lastName ='Florez' // Redeclarar
+console.log(lastName);
+
+var secondName = 'Ricardo'; // Declarando / Asignado
+var secondName = 'David'; // Reasignando
+
+//Let
+let fruit = 'Pinapple'; // Declarar y asignar
+fruit = 'Strawberry'; // Reasignar
+console.log(fruit);
+
+let fruit = 'Banana'; 
+console.log(fruit);
+
+//Const
+const animal = 'Lion'; // Declarar y asignar
+animal = 'Tiger';   // Reasignar
+const animal = 'Puma'; // Redeclarar
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('audi');
+console.log(vehicles)
+
+vehicles.pop();
+console.log(vehicles)
